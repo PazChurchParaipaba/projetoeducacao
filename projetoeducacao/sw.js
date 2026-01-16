46db5926ea6d02b122f2a87fe32d0fab24750c3d
PazChurchParaipaba <erickbarroso677@gmail.com>
2026-01-16 13:31:05 -0300

    Delete projetoeducacao directory

--- projetoeducacao/sw.js
+++
@@ -1,9 +0,0 @@
-// sw.js
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Instalado');
-});
-
-self.addEventListener('fetch', (e) => {
-    // Por enquanto, apenas responde com a rede normal
-    e.respondWith(fetch(e.request));
-});
